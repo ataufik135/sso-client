5ccdf88f051cef2a9737509bc27cc32ad3f9420e
Taufik <ataufik135@gmail.com>
2023-10-07 06:37:29 +0700

    1

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,8 +5,8 @@ use TaufikT\SsoClient\Http\Controllers\SSOController;
 
 Route::controller(SSOController::class)->group(function () {
   Route::prefix('/oauth2')->group(function () {
-    Route::get('/redirect', 'redirect')->name('oauth2.redirect')->middleware('sso.guest');
-    Route::get('/callback', 'callback')->name('oauth2.callback')->middleware('sso.guest');
-    Route::get('/logout', 'logout')->name('oauth2.logout')->middleware('sso.auth');
+    Route::get('/redirect', 'redirect')->name('oauth2.redirect')->middleware(['web', 'sso.guest']);
+    Route::get('/callback', 'callback')->name('oauth2.callback')->middleware(['web', 'sso.guest']);
+    Route::get('/logout', 'logout')->name('oauth2.logout')->middleware(['web', 'sso.auth']);
   });
 });
