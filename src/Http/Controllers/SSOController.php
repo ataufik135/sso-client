4670301271050b5597c75a115383fc03e13df188
Taufik <ataufik135@gmail.com>
2024-06-09 20:52:35 +0700

    .

--- src/Http/Controllers/SSOController.php
+++ src/Http/Controllers/SSOController.php
@@ -3,77 +3,82 @@
 namespace TaufikT\SsoClient\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Str;
 use TaufikT\SsoClient\OAuthClient;
 
 class SSOController
 {
-  protected $oauthClient;
+    protected $oauthClient;
 
-  public function __construct(OAuthClient $oauthClient)
-  {
-    $this->oauthClient = $oauthClient;
-  }
-
-  public function redirect(Request $request)
-  {
-    $request->session()->put('request_url', $request->session()->get('_previous.url'));
-    $request->session()->put('request_ip', $request->ip());
-    $request->session()->put('state', $state = Str::random(40));
-    $request->session()->put('code_verifier', $code_verifier = Str::random(128));
-    $codeChallenge = strtr(rtrim(base64_encode(hash('sha256', $code_verifier, true)), '='), '+/', '-_');
-
-    $query = http_build_query([
-      'response_type' => 'code',
-      'client_id' => $this->oauthClient->clientId(),
-      'redirect_uri' => $this->oauthClient->redirectUri(),
-      'scope' => $this->oauthClient->scopes(),
-      'state' => $state,
-      'code_challenge' => $codeChallenge,
-      'code_challenge_method' => 'S256',
-    ]);
-
-    return redirect($this->oauthClient->host() . '/oauth/authorize?' . $query);
-  }
-  public function callback(Request $request)
-  {
-    $requestUrl = $request->session()->pull('request_url');
-    $requestIp = $request->session()->pull('request_ip');
-    $state = $request->session()->pull('state');
-    $codeVerifier = $request->session()->pull('code_verifier');
-
-    if (strlen($state) < 1 && $state !== $request->state) {
-      return response()->json(['message' => 'Invalid state value.'], 400);
+    public function __construct(OAuthClient $oauthClient)
+    {
+        $this->oauthClient = $oauthClient;
     }
 
-    try {
-      $this->oauthClient->requestToken($request->code, $codeVerifier, $requestIp);
-      $user = $request->session()->get('user');
+    public function redirect(Request $request)
+    {
+        $request->session()->put('request_url', $request->session()->get('_previous.url'));
+        $request->session()->put('request_ip', $request->ip());
+        $request->session()->put('state', $state = Str::random(40));
+        $request->session()->put('code_verifier', $code_verifier = Str::random(128));
+        $codeChallenge = strtr(rtrim(base64_encode(hash('sha256', $code_verifier, true)), '='), '+/', '-_');
 
-      $request->session()->regenerate();
-      $this->oauthClient->addAuthUser($user['id'], $user['sessionId']);
-      return $requestUrl !== null ? redirect($requestUrl) : redirect()->intended('/');
-    } catch (\Exception $e) {
-      return response()->json(['message' => 'Unauthorized.'], 403);
+        $query = http_build_query([
+            'response_type' => 'code',
+            'client_id' => $this->oauthClient->clientId(),
+            'redirect_uri' => $this->oauthClient->redirectUri(),
+            'scope' => $this->oauthClient->scopes(),
+            'state' => $state,
+            'code_challenge' => $codeChallenge,
+            'code_challenge_method' => 'S256',
+        ]);
+
+        return redirect($this->oauthClient->host() . '/oauth/authorize?' . $query);
     }
-  }
+    public function callback(Request $request)
+    {
+        $requestUrl = $request->session()->pull('request_url');
+        $requestIp = $request->session()->pull('request_ip');
+        $state = $request->session()->pull('state');
+        $codeVerifier = $request->session()->pull('code_verifier');
 
-  public function logout(Request $request)
-  {
-    $request->session()->invalidate();
-    $request->session()->regenerateToken();
-    return redirect($this->oauthClient->logoutUri());
-  }
+        if (strlen($state) < 1 && $state !== $request->state) {
+            return response()->json(['message' => 'Invalid state value.'], 400);
+        }
 
-  public function handleLogoutNotification(Request $request)
-  {
-    $token = $request->header('Authorization');
-    if (!$token) {
-      return response()->json(['message' => 'Unauthorized.'], 403);
+        try {
+            $response = $this->oauthClient->requestToken($request->code, $codeVerifier, $requestIp);
+            if ($response->status() === 400) {
+                return $response->json();
+            }
+            if ($response->status() === 403) {
+                abort(403);
+            }
+            $user = $request->session()->get('user');
+
+            $request->session()->regenerate();
+            $this->oauthClient->addAuthUser($user['id'], $user['sessionId']);
+            return $requestUrl !== null ? redirect($requestUrl) : redirect()->intended('/');
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Unauthorized.'], 403);
+        }
     }
 
-    $this->oauthClient->logout($token);
-    return response()->json(['message' => 'You have been logged out!'], 200);
-  }
+    public function logout(Request $request)
+    {
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect($this->oauthClient->logoutUri());
+    }
+
+    public function handleLogoutNotification(Request $request)
+    {
+        $token = $request->header('Authorization');
+        if (!$token) {
+            return response()->json(['message' => 'Unauthorized.'], 403);
+        }
+
+        $this->oauthClient->logout($token);
+        return response()->json(['message' => 'You have been logged out!'], 200);
+    }
 }
--- src/OAuthClient.php
+++ src/OAuthClient.php
@@ -2,7 +2,6 @@
 
 namespace TaufikT\SsoClient;
 
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Http;
@@ -12,172 +11,173 @@ use GuzzleHttp\Promise;
 
 class OAuthClient
 {
-  protected $host;
-  protected $logoutUri;
-  protected $clientId;
-  protected $clientSecret;
-  protected $redirectUri;
-  protected $clientOrigin;
-  protected $scopes;
+    protected $host;
+    protected $logoutUri;
+    protected $clientId;
+    protected $clientSecret;
+    protected $redirectUri;
+    protected $clientOrigin;
+    protected $scopes;
+    protected $version;
 
-  public function __construct()
-  {
-    $this->host = Config::get('sso.host');
-    $this->logoutUri = Config::get('sso.host_logout');
-    $this->clientId = Config::get('sso.client_id');
-    $this->clientSecret = Config::get(('sso.client_secret'));
-    $this->redirectUri = Config::get('sso.client_callback');
-    $this->clientOrigin = Config::get('sso.client_origin');
-    $this->scopes = Config::get('sso.scopes');
-  }
-
-  public function addAuthUser($userId, $sessionId)
-  {
-    $users = Cache::get('authenticated-users');
-
-    $users[$userId] = ['sessionId' => $sessionId];
-    Cache::forever('authenticated-users', $users);
-  }
-  public function removeAuthUser($userId)
-  {
-    $users = cache::get('authenticated-users');
-    unset($users[$userId]);
-    Cache::forever('authenticated-users', $users);
-  }
-  public function countAuthUser()
-  {
-    $users = cache::get('authenticated-users');
-    return count($users);
-  }
-  public function getSessionIdAuthUser($userId)
-  {
-    $sessionId = cache::get('authenticated-users')[$userId]['sessionId'];
-    return $sessionId;
-  }
-  public function getAllUserIdAuthUser()
-  {
-    return array_keys(cache::get('authenticated-users'));
-  }
-  public function checkAuthUser($userId)
-  {
-    return isset(Cache::get('authenticated-users')[$userId]);
-  }
-  public function updateOnlineUsers($data)
-  {
-    $httpClient = new Client([
-      'http_errors' => false,
-      'verify' => false
-    ]);
-    $promises = $httpClient->postAsync(
-      $this->host . '/api/online-users',
-      [
-        'headers' => [
-          'Accept' => 'application/json',
-          'client-id' => $this->clientId
-        ],
-        'form_params' => ['online_users' => $data]
-      ]
-
-    );
-
-    Promise\Utils::settle($promises)->wait();
-  }
-
-  public function storeToken($data)
-  {
-    Session::put($data);
-  }
-  public function requestToken($code, $codeVerifier, $requestIp)
-  {
-    $response = Http::withoutVerifying()->withHeaders([
-      'X-Forwarded-For' => $requestIp,
-    ])->acceptJson()->asForm()->post(
-      $this->host . '/api/oauth/token',
-      [
-        'grant_type' => 'authorization_code',
-        'client_id' => $this->clientId,
-        'client_secret' => $this->clientSecret,
-        'redirect_uri' => $this->redirectUri,
-        'code_verifier' => $codeVerifier,
-        'code' => $code
-      ]
-    );
-
-    if ($response->status() === 409) {
-      return redirect($this->logoutUri);
+    public function __construct()
+    {
+        $this->host = Config::get('sso.host');
+        $this->logoutUri = Config::get('sso.host_logout');
+        $this->clientId = Config::get('sso.client_id');
+        $this->clientSecret = Config::get(('sso.client_secret'));
+        $this->redirectUri = Config::get('sso.client_callback');
+        $this->clientOrigin = Config::get('sso.client_origin');
+        $this->scopes = Config::get('sso.scopes');
+        $this->version = Config::get('sso.version');
     }
 
-    $this->storeToken($response->json());
-    if ($response->status() === 403) {
-      abort(403);
+    public function addAuthUser($userId, $sessionId)
+    {
+        $users = Cache::get('authenticated-users');
+
+        $users[$userId] = ['sessionId' => $sessionId];
+        Cache::forever('authenticated-users', $users);
     }
-  }
+    public function removeAuthUser($userId)
+    {
+        $users = cache::get('authenticated-users');
+        unset($users[$userId]);
+        Cache::forever('authenticated-users', $users);
+    }
+    public function countAuthUser()
+    {
+        $users = cache::get('authenticated-users');
+        return count($users);
+    }
+    public function getSessionIdAuthUser($userId)
+    {
+        $sessionId = cache::get('authenticated-users')[$userId]['sessionId'];
+        return $sessionId;
+    }
+    public function getAllUserIdAuthUser()
+    {
+        return array_keys(cache::get('authenticated-users'));
+    }
+    public function checkAuthUser($userId)
+    {
+        return isset(Cache::get('authenticated-users')[$userId]);
+    }
+    public function updateOnlineUsers($data)
+    {
+        $httpClient = new Client([
+            'http_errors' => false,
+            'verify' => false
+        ]);
+        $promises = $httpClient->postAsync(
+            $this->host . '/api/online-users',
+            [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'client-id' => $this->clientId
+                ],
+                'form_params' => ['online_users' => $data]
+            ]
 
-  public function refreshToken()
-  {
-    $refresh_token = Session::get('refresh_token');
-    if (!$refresh_token) {
-      return redirect($this->clientOrigin);
+        );
+
+        Promise\Utils::settle($promises)->wait();
     }
 
-    $response = Http::withoutVerifying()->acceptJson()->asForm()->post(
-      $this->host . '/api/oauth/token',
-      [
-        'grant_type' => 'refresh_token',
-        'refresh_token' => $refresh_token,
-        'client_id' => $this->clientId,
-        'client_secret' => $this->clientSecret,
-        'scope' => $this->scopes,
-      ]
-    );
+    public function storeToken($data)
+    {
+        Session::put($data);
+    }
+    public function requestToken($code, $codeVerifier, $requestIp)
+    {
+        $response = Http::withoutVerifying()->withHeaders([
+            'X-Forwarded-For' => $requestIp,
+            'client-version' => $this->version,
+        ])->acceptJson()->asForm()->post(
+            $this->host . '/api/oauth/token',
+            [
+                'grant_type' => 'authorization_code',
+                'client_id' => $this->clientId,
+                'client_secret' => $this->clientSecret,
+                'redirect_uri' => $this->redirectUri,
+                'code_verifier' => $codeVerifier,
+                'code' => $code
+            ]
+        );
 
-    return $response->json();
-  }
+        if ($response->status() === 409) {
+            return redirect($this->logoutUri);
+        }
 
-  public function logout($token)
-  {
-    $token = base64_decode($token);
+        $this->storeToken($response->json());
+        return $response;
+    }
 
-    $key = Config::get('sso.key');
-    $encrypt = new \Illuminate\Encryption\Encrypter($key, 'AES-256-CBC');
-    $decrypted = $encrypt->decrypt($token);
+    public function refreshToken()
+    {
+        $refresh_token = Session::get('refresh_token');
+        if (!$refresh_token) {
+            return redirect($this->clientOrigin);
+        }
 
-    $this->destroySessionById($decrypted);
-  }
+        $response = Http::withoutVerifying()->acceptJson()->asForm()->post(
+            $this->host . '/api/oauth/token',
+            [
+                'grant_type' => 'refresh_token',
+                'refresh_token' => $refresh_token,
+                'client_id' => $this->clientId,
+                'client_secret' => $this->clientSecret,
+                'scope' => $this->scopes,
+            ]
+        );
 
-  private function destroySessionById($userId)
-  {
-    $sessionId = $this->getSessionIdAuthUser($userId);
-    $this->removeAuthUser($userId);
-    Session::getHandler()->destroy($sessionId);
-  }
+        return $response->json();
+    }
 
-  public function host()
-  {
-    return $this->host;
-  }
-  public function logoutUri()
-  {
-    return $this->logoutUri;
-  }
-  public function clientId()
-  {
-    return $this->clientId;
-  }
-  public function clientSecret()
-  {
-    return $this->clientSecret;
-  }
-  public function redirectUri()
-  {
-    return $this->redirectUri;
-  }
-  public function clientOrigin()
-  {
-    return $this->clientOrigin;
-  }
-  public function scopes()
-  {
-    return $this->scopes;
-  }
+    public function logout($token)
+    {
+        $token = base64_decode($token);
+
+        $key = Config::get('sso.key');
+        $encrypt = new \Illuminate\Encryption\Encrypter($key, 'AES-256-CBC');
+        $decrypted = $encrypt->decrypt($token);
+
+        $this->destroySessionById($decrypted);
+    }
+
+    private function destroySessionById($userId)
+    {
+        $sessionId = $this->getSessionIdAuthUser($userId);
+        $this->removeAuthUser($userId);
+        Session::getHandler()->destroy($sessionId);
+    }
+
+    public function host()
+    {
+        return $this->host;
+    }
+    public function logoutUri()
+    {
+        return $this->logoutUri;
+    }
+    public function clientId()
+    {
+        return $this->clientId;
+    }
+    public function clientSecret()
+    {
+        return $this->clientSecret;
+    }
+    public function redirectUri()
+    {
+        return $this->redirectUri;
+    }
+    public function clientOrigin()
+    {
+        return $this->clientOrigin;
+    }
+    public function scopes()
+    {
+        return $this->scopes;
+    }
 }
